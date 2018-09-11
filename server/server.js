982aa26427ebd2d194b78994b759807a67c557f6
Ido Yaari <suzyido@yahoo.com>
2018-09-11 21:31:46 +0300

    added PATCH /todos

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -67,6 +68,29 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((err) => {
         res.status(404).send();
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((e) => res.status(400).send());
 });
 
 if(!module.parent) { // This is here to avoid errors when running Mocha (double listening)
