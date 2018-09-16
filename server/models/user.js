be96af1bcfd02885d805586c7c74e72e1149a8d6
Ido Yaari <suzyido@yahoo.com>
2018-09-16 13:48:31 +0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -44,7 +44,6 @@ UserSchema.methods.generateAuthToken = function() {
     var user = this;
     var access = 'auth';
     var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
-//    user.tokens = user.tokens.concat([{access, token}]);
     user.tokens.push({access, token});
     return user.save().then(() => {
         return token;
@@ -67,6 +66,31 @@ UserSchema.statics.findByToken = function(token) {
         'tokens.token': token,
         'tokens.access': 'auth'
     })
+}
+
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+    console.log('In findByCredentials');
+    console.log(email);
+
+    return User.findOne({email}).then((user) => {
+        if(!user) {
+            return Promise.reject();
+        }
+        
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res) {
+                    resolve(user);
+                }
+                else {
+                    reject();
+                }
+            });            
+        });    
+    }).catch((err) => {
+            return Promise.reject();
+    });
 }
 
 UserSchema.pre('save', function(next) {
